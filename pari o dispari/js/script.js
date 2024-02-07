700047284439d6492bc0a2e764ca51ac60f6879a
federico-dibitonto <f.dibitonto94@gmail.com>
2024-02-07 03:47:54 +0100

    Exercise Completed

--- pari o dispari/js/script.js
+++ pari o dispari/js/script.js
@@ -1,5 +1,9 @@
 let resultElement = document.querySelector("#result");
+let titleElement = document.querySelector("#text");
 
+/* Creo le funzioni e ne verifico l'immissione dei dati */ 
+
+/* Inserimento numero da 1 a 5 da parte dell'user */ 
 function insertNumber() {
     let myNumber;
 
@@ -7,6 +11,8 @@ function insertNumber() {
         
         myNumber = prompt('Inserisci un numero da 1 a 5');
 
+        /* Verifico che il dato non sia un numero e che non sia un valore minore di 1 o maggiore di 5,
+            e ne prendo il risultato */ 
         if (isNaN(myNumber) || (myNumber < 1 || myNumber > 5)) {
             
             alert('Il valore inserito non è valido riprova');
@@ -21,6 +27,7 @@ function insertNumber() {
     } while (!(myNumber >= 1 && myNumber <= 5));
 }
 
+/* Creo un numero casuale da 1 a 5 e ne prendo il risultato */ 
 function pcNumber() {
 
     let randomNumber = (Math.floor(Math.random() *5) + 1);
@@ -29,7 +36,7 @@ function pcNumber() {
 
 }
 
-
+/* Sommo i due valori in memoria */ 
 function sum (a, b) {
     let sum = a + b;
 
@@ -39,7 +46,7 @@ function sum (a, b) {
 
 ///////////////////////////////////////////////////////////////////////////
 
-
+/* Creo la variabile con la richiesta se Pari o la somma sarà pari o dispari */ 
 let evenodd;
 
 do {
@@ -50,40 +57,36 @@ do {
             
         alert('Il valore inserito non è valido riprova');
 
-    } else {
-
-        console.log(evenodd)
-
     } 
 
 } while (!(evenodd == 'pari' || evenodd == 'dispari'));
 
 
-
+/* Creiamo le variabili ed effettuiamo la somma */ 
 let userNumber = insertNumber();
 let ranNumber = pcNumber();
-console.log(userNumber);
-console.log(ranNumber);
+
 sum(userNumber, ranNumber);
-console.log(sum(userNumber, ranNumber));
-let pari;
-let dispari;
 
+let autosum;
+
+
+/* Verifichiamo se la somma è pari o dispari */ 
 if (sum(userNumber, ranNumber) % 2 == 0){
-    pari = ('il numero è pari');
+    autosum = ('il somma é pari');
 } else {
-    dispari = ('il numero è dispari');
+    autosum = ('il somma é dispari');
 }
 
-let vittoria;
-let sconfitta;
+/* Confermiamo se l'user ha vinto o perso e ne diamo il risultato */ 
+let epilogue;
 
-if (sum(userNumber, ranNumber) % 2 == 0 && evenodd == 'pari'){
-    vittoria = ('bravo è pari, hai indovinato');
-} else if (sum(userNumber, ranNumber) % 2 !== 0 && evenodd == 'dispari') {
-    vittoria = ('bravo è dispari, hai indovinato');
+if ((sum(userNumber, ranNumber) % 2 == 0 && evenodd == 'pari') || (sum(userNumber, ranNumber) % 2 !== 0 && evenodd == 'dispari')){
+    epilogue = ('Bravo, hai indovinato');
 } else {
-    sconfitta = ('sei una merda, hai perso');
+    epilogue = ('Mi dispace, hai perso');
 }
 
-resultElement.innerHTML = `Il numero da te scelto è ${userNumber}, il computer ha scelto ${ranNumber}, ${pari || dispari}, ${vittoria || sconfitta}`;
+/* Mostriamo a schermo il risultato */ 
+titleElement.innerHTML = `Il numero da te scelto è ${userNumber}, il computer ha scelto ${ranNumber}, ${autosum}`;
+resultElement.innerHTML = `${epilogue}`;
--- parole palindrome/js/script.js
+++ parole palindrome/js/script.js
@@ -1,34 +1,36 @@
 let resultElement = document.querySelector("#result");
-let parola;
+let word;
+let result;
 
+/* Creo funzione di controllo delle parole palindrome */ 
 function inspect() {
 
     
     let text = false;
     
+    /* Inserisco un ciclo che confronti tutte le lettere della parola inserita */ 
     while (text == false) {
         
-        parola = prompt('Inserisci una parola palindroma');
+        word = prompt('Capisco se la parola che inserisci è palindroma');
 
-        if (!isNaN(parola)){
+        if (!isNaN(word)){
         
             alert('Hai inserito un valore non valido riprova');
         
         } else {
             
-            for (let i = 0; i < Math.floor(parola.length / 2); i++){
-            
-                console.log(parola[i]);
-                console.log(parola[parola.length - (1 + i)]);
+            for (let i = 0; i < Math.floor(word.length / 2); i++){
     
-                if (!(parola[i] == parola[parola.length - (1 + i)])){
+                if (!(word[i] == word[word.length - (1 + i)])){
                     
+                    result = `La parola non é palindroma`
                     return ('la parola non è palindroma');
                 
                 }
     
             }
-    
+            
+            result = `La parola é palindroma`
             text == true;
             return ('la parola è palindroma');      
 
@@ -39,4 +41,8 @@ function inspect() {
 
 }
 
-console.log(inspect());
+
+/* Lancio la funzione e ne mostro a schermo il risultato */ 
+inspect();
+
+resultElement.innerHTML = `${result}`;
